a27b70ae12fd2e1a574a222f48b38b18f6b71fad
ckorir <chrisruto.k@gmail.com>
2023-02-16 13:39:27 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * main - "Programming is like building a multilingual puzzle
+ * main - prints "Programming is like building a multilingual puzzle
  * followed by a new line." 
  *
  * Return: Always 0

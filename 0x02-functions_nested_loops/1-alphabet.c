282875e6db6c3f6e6a0d2be694b25b9bbabc13d8
ckorir <chrisruto.k@gmail.com>
2023-02-21 12:28:19 -0800

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase
+ *
+ * Return: always 0
+ */
+void print_alphabet(void);
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}

d69712765063875e1bc4708e6020baf520581d3c
ckorir <chrisruto.k@gmail.com>
2023-02-21 12:43:33 -0800

    prints the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,13 @@
  *
  * Return: always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
+	_putchar(i);
 	}
 	_putchar('\n');
 }

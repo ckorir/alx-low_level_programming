67f92e2b92569640dc9795e4a7b1f00211ce89c4
ckorir <chrisruto.k@gmail.com>
2023-02-21 16:57:52 -0800

    finds and prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - finds and prints the first 98 Fibonacci numbers, starting with 1 and 2
- * return: always 0
+ * main - finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2
+ * Return: always 0
  */
 int main(void)
 {

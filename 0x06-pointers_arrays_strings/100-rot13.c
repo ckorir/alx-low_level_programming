fcd363d4c2bbdc7eae4f323893a3d0f5f74e50a9
ckorir <61984170+ckorir@users.noreply.github.com>
2023-03-03 19:58:53 +0300

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,6 +14,7 @@ char *rot13(char *s)
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxys";
 	char datrot[] =
 	"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	
 	for (i = 0; s[i] != '\0'; i++)
 	{
 	for (j = 0; j < 52; j++)

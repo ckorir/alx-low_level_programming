953e068fe8f11b98d439de373896f627d678b48f
ckorir <chrisruto.k@gmail.com>
2023-03-10 06:10:28 -0800

    functions listed

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * _putchar - prints char
+ * @c: Char to be printed
+ * Return: char
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
